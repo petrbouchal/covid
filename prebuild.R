284023d42d1f6b99862cddd1309c108f031fb177
Petr Bouchal <pbouchal@gmail.com>
2020-11-08 15:44:50 +0100

    save pre-built inputs as RDS to avoid column format issues

--- prebuild.R
+++ prebuild.R
@@ -3,10 +3,10 @@ library(czso)
 library(CzechData)
 
 vek <- czso::czso_get_table("130142")
-write_csv(vek, "vek.csv")
+write_rds(vek, "vek.rds")
 
 vek_ciselnik <- czso::czso_get_codelist("cis7700")
-write_csv(vek_ciselnik, "vek_ciselnik.csv")
+write_rds(vek_ciselnik, "vek_ciselnik.rds")
 
 okresy_p <- CzechData::load_RUIAN_state("okresy") %>%
   ms_simplify(keep = .01, keep_shapes = T)
