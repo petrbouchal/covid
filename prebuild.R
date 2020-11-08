ee73938577c956d68a48412f7b1fa1ba97ddda46
Petr Bouchal <pbouchal@gmail.com>
2020-11-08 15:10:42 +0100

    add mortality chart adjusted for age structure

--- prebuild.R
+++ prebuild.R
@@ -2,6 +2,11 @@ library(tidyverse)
 library(czso)
 library(CzechData)
 
+vek <- czso::czso_get_table("130142")
+write_csv(vek, "vek.csv")
+
+vek_ciselnik <- czso::czso_get_codelist("cis7700")
+write_csv(vek_ciselnik, "vek_ciselnik.csv")
 
 okresy_p <- CzechData::load_RUIAN_state("okresy") %>%
   ms_simplify(keep = .01, keep_shapes = T)
