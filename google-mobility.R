0584a0ae973b9cbef53ba415d46b0ddf0139b326
Petr Bouchal <pbouchal@gmail.com>
2020-12-11 00:23:37 +0100

    code for mobility chart

--- google-mobility.R
+++ google-mobility.R
@@ -1,7 +1,14 @@
 source("google_load-transform.R")
 
 gd_dlouhe %>% 
-  filter(kraj_nazev == "Celá ČR") %>% 
+  filter(kraj_nazev == "Celá ČR",
+         date > "2020-09-01") %>% 
   ggplot(aes(date, zmena)) +
-  geom_line(aes(colour = kategorie_cz), size = .5) +
-  facet_wrap(~ kategorie)
+  geom_line(size = .5, colour = "grey") +
+  geom_smooth(aes(colour = kategorie_cz), size = .5, show.legend = F) +
+  facet_wrap(~ kategorie_cz) +
+  theme_ptrr("both", multiplot = T) +
+  labs(title = "Mobilita podle typu místa",
+       subtitle = str_glue("100 = před-Covid průměr pro daný den v týdnu\n",
+                           "šedá čára = data, barevná = Loess vyhlazení"),
+       caption = "@ petrbouchal z dat Google mobility")
